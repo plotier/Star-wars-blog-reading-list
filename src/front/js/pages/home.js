7f2472b0c8b4edcbf54d70b0d20b6064b9788d90
plotier <plotier15@gmail.com>
2021-06-23 18:54:39 +0000

    primera card

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,7 +8,20 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
+			<h1>Star Wars¿?</h1>
+			<div className="card" style={{ width: "18rem" }}>
+				<img src="..." className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">
+						Some quick example text to build on the card title and make up the bulk of the cards content.
+					</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+
 			<p>
 				<img src={rigoImageUrl} />
 			</p>
